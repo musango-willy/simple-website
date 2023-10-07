04c14ba5f7f03149173224998082487ec3cd4216
musango willy <musangowilly@gmail.com>
2023-10-07 22:57:09 +0300

    wrote a comment

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ const switcher = document.querySelector('.btn');
 switcher.addEventListener('click', function() {
     document.body.classList.toggle('light-theme');
     document.body.classList.toggle('dark-theme');
+    // describes how to switch themes in the app
 
     const className = document.body.className;
     if(className == "light-theme") {
