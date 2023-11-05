1e2719a5003509e672dd8242599a25e5bee39519
musango willy <musangowilly@gmail.com>
2023-11-05 13:00:20 +0300

    learning objects in javascript

---
+++ object.js
@@ -0,0 +1,16 @@
+//
+//object constructors 
+
+const person={
+    firstname:"wycliffe",
+    middlename:"musango",
+    lastname:"willy",
+    age:23,
+    email:"musangowilly@gmail.com",
+    hobbies:["football","movies","cycling"],
+    address:{
+        streetname:"4th street",
+        no:7,
+        city:"nairobi",
+    }
+ 
