65e21bbb63fe28fb27cb05231fdf6753eac87117
musango willy <musangowilly@gmail.com>
2023-11-05 13:02:37 +0300

    ook

--- object.js
+++ object.js
@@ -13,4 +13,34 @@ const person={
         no:7,
         city:"nairobi",
     }
- 
+    const name="wycliife"
+}
+js.prototype.getFullname=function(){
+    console.log(wycliffe);
+}
+const wycliffe=new js("wycliffe","musango","willy",23,"musangowilly@gmail.com");
+
+const lewis=new js("lewis","machabe",37,"lewis@gmail.com");
+
+const jsArr=[sharon,lewis]
+
+
+//new object constructors and prototype
+
+//object constructors 
+
+function js(firstname,lastname,age,email){
+
+    this.firstname=firstname;
+    this.lastname=lastname;
+    this.age=age;
+    this.email=email;
+}
+
+js.prototype.getFullname=function(){
+    return this.firstname + " " + this.lastname;
+}
+const name="sharon";
+
+function call(){
+    console.log(name)
